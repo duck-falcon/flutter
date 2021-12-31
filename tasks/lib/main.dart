9e5aeed14f2f5d0b316d9bf32781d404668595de
midorikawa <midorikawa@iij.ad.jp>
2021-12-31 22:35:33 +0900

    画面遷移

--- tasks/lib/main.dart
+++ tasks/lib/main.dart
@@ -11,17 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/homer': (context) =>
+            const MyHomePage(title: 'Flutter Demo Home Page'),
+        '/stateless': (context) => const StatelesPage(),
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -29,17 +26,30 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class StatelesPage extends StatelessWidget {
+  const StatelesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("StatelesPage"),
+        ),
+        body: Center(
+            child: Column(children: <Widget>[
+          const Text("書き換えしないページ"),
+          TextButton(
+            child: const Text("home page link"),
+            onPressed: () {
+              Navigator.pushNamed(context, '/home');
+            },
+          )
+        ])));
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -53,11 +63,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -118,7 +123,13 @@ class _MyHomePageState extends State<MyHomePage> {
                       color: Colors.blue,
                       size: 36.0,
                     ),
-                  ])
+                  ]),
+              TextButton(
+                child: const Text("stateless page link"),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/stateless');
+                },
+              ),
             ]),
       ),
       floatingActionButton: FloatingActionButton(
