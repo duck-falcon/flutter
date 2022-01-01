e9d33d734fe31879a97949079abae7eeded108a0
midorikawa <midorikawa@iij.ad.jp>
2022-01-01 15:25:02 +0900

    page 遷移実装

--- tasks/lib/main.dart
+++ tasks/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'stateless.dart';
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       initialRoute: '/home',
       routes: {
-        '/homer': (context) =>
-            const MyHomePage(title: 'Flutter Demo Home Page'),
-        '/stateless': (context) => const StatelesPage(),
+        '/home': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
+        '/stateless': (context) => const StatelessPage(),
       },
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -23,28 +24,6 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
-
-class StatelesPage extends StatelessWidget {
-  const StatelesPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("StatelesPage"),
-        ),
-        body: Center(
-            child: Column(children: <Widget>[
-          const Text("書き換えしないページ"),
-          TextButton(
-            child: const Text("home page link"),
-            onPressed: () {
-              Navigator.pushNamed(context, '/home');
-            },
-          )
-        ])));
   }
 }
 
@@ -58,85 +37,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  bool _anivFlag = true;
+  late PageController _pageController;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  // ボトムメニューの遷移先画面, 上から0,1,...
+  final _pages = [
+    const Home(title: 'Flutter Demo Home Page'),
+    const StatelessPage(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _selectedIndex);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  void _onPageChanged(int index) {
     setState(() {
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    //return LoginPage();
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      drawer: Drawer(
-          child: ListView(children: <Widget>[
-        /* コンテナでヘッダ部分を作る */
-        SwitchListTile(
-          title: const Text('記念日'),
-          value: _anivFlag,
-          onChanged: (bool value) {
-            setState(() {
-              _anivFlag = value;
-            });
-          },
-          secondary: const Icon(Icons.favorite, color: Colors.teal),
-        ),
-        /* SwitichListTileが続く */
-        /* Dividerなど他のものが含まれてもOK */
-      ])),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              const Text("2番目のテキスト"),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Icon(
-                      Icons.favorite,
-                      color: Colors.pink,
-                      size: 24.0,
-                    ),
-                    Icon(
-                      Icons.audiotrack,
-                      color: Colors.green,
-                      size: 30.0,
-                    ),
-                    Icon(
-                      Icons.beach_access,
-                      color: Colors.blue,
-                      size: 36.0,
-                    ),
-                  ]),
-              TextButton(
-                child: const Text("stateless page link"),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/stateless');
-                },
-              ),
-            ]),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: PageView(
+            controller: _pageController,
+            onPageChanged: _onPageChanged,
+            children: _pages));
   }
 }
